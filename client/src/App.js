2ff1a1ca27f74d704e10704e1a07fe10b6379381
Sithu Kyaw <sithukyaw27500@gmail.com>
2021-08-16 14:37:01 +0630

    added route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
-
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/api");
+      console.log(await response.json());
+    })();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "build")));
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.sendFile(path.join("index.html"));
+});
+app.get("/api", (req, res) => {
+  res.json({
+    user: "James Bond",
+  });
 });
 app.listen(PORT, () => console.log("server is up "));
